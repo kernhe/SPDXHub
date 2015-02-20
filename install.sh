7213854183544a451c9e9a9d28012f383ec71261
joverkamp <jmoverkamp@gmail.com>
2015-02-20 11:42:29 -0600

    Update install.sh
    Updating install.sh file to grab Dashboard from our forked directory. Will need to modify once mroe once we begin testing in dev environment.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ p=$PWD
 #Clone Repos
 git clone https://github.com/socs-dev-env/SOCSDatabase
 git clone https://github.com/socs-dev-env/DoSOCS
-git clone https://github.com/socs-dev-env/SOCSDashboard
+git clone https://github.com/jmoverkamp/SPDXHub
 
 #Install Database
 echo "Install SOCS Database..."
@@ -16,27 +16,27 @@ mysql --user=$u --password=$p < SOCSDatabase/SQL/SPDX.sql
 
 #Create Upload Directory
 echo "Creating Upload Directory..."
-sudo mkdir SOCSDashboard/uploads
-sudo chmod 777 SOCSDashboard/uploads
+sudo mkdir SPDXHub/uploads
+sudo chmod 777 SPDXHub/uploads
 
 
 #Move source to base directorie
 echo "Reconfigure Repo Folder Structure"
 sudo mv DoSOCS/src/* DoSOCS/
-sudo mv SOCSDashboard/src/* SOCSDashboard/
+sudo mv SPDXHub/src/* SPDXHub/
 sudo chmod 777 DoSOCS/ -R
 
 #Remove src directories
 echo "Removing old Repo Directories"
 sudo rm DoSOCS/src -R
-sudo rm SOCSDashboard/src -R
+sudo rm SPDXHub/src -R
 
 #Delete Database Repo
 echo "Remove Database Repo"
 sudo rm SOCSDatabase -R
 
 echo "Install Complete"
-echo "Don't forget to update the setting files ('DoSOCS/settings.py' AND 'SOCSDashboard/function/Data_Source.php') with the database"
+echo "Don't forget to update the setting files ('DoSOCS/settings.py' AND 'SPDXHub/function/Data_Source.php') with the database"
 echo "connection information, and with the paths to Ninka and FOSSology."
 
 
