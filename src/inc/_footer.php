2583b35ddae8d68f6ee5ee6c6316833f81d0eeaf
nchickinelli <nchickinelli@gmail.com>
2015-02-28 20:43:56 -0600

    add _footer include

---
+++ src/inc/_footer.php
@@ -0,0 +1,11 @@
+        
+
+        <div class="container col-md-3 col-md-offset-5">
+            <footer>
+                <p>&copy; University of Nebraska at Omaha 2014<p>
+            </footer>
+        </div>
+    </body>
+</html>
+  
+    
