1fb902142446abd231cfec83dd9aea68c4404f45
nchickinelli <nchickinelli@gmail.com>
2015-02-28 22:07:37 -0600

    remove include comment

--- src/index.php
+++ src/index.php
@@ -15,8 +15,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -->
 <?php
-    // include("function/headerfooter.php");
-    
     include("inc/_header.php");
     include("function/spdx_doc.php");
     $name = "";
