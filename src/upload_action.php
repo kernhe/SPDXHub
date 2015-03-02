60e3610d6b9867bcbe0bfbb0406090e66fad541d
Jacob Overkamp <jmoverkamp@gmail.com>
2015-03-02 08:44:09 -0600

    Gave all variables in upload_action.php a logical OR assignment to allow document files to be uploaded in the same file as package scans.

--- src/upload_action.php
+++ src/upload_action.php
@@ -16,70 +16,81 @@ limitations under the License.
 (isset($_POST['scan_option']) ? $_POST['scan_option'] : null);
 -->
 <?php
-	include("function/headerfooter.php");
+	$title = "Upload";
+	include("inc/_header.php");
 	include("function/Data_Source.php");
-	incHeader("Upload");
-    $filePath                   = $_FILES["package"]["tmp_name"];
-    $fileName                   = $_FILES["package"]["name"];
-    $document_comment           = $_POST['document_comment'];
-    $creator                    = $_POST['creator'];
-    $creator_comment            = $_POST['creator_comment'];
-    $pakage_version             = $_POST['pacakge_version'];
-    $package_supplier           = $_POST['package_supplier'];
-    $package_originator         = $_POST['package_originator'];
-    $package_download_location  = $_POST['package_download_location'];
-    $package_home_page          = $_POST['package_home_page'];
-    $package_source_info        = $_POST['package_source_info'];
-    $package_license_comments   = $_POST['package_license_comments'];
-    $package_description        = $_POST['package_description'];
+    $packagefilePath            = (isset($_FILES['package']["tmp_name"]) ? $_FILES['package']["tmp_name"] : null);
+    $packagefileName            = (isset($_FILES['package']["name"]) ? $_FILES['package']["name"] : null);
+    $docfilePath                = (isset($_FILES['document']["tmp_name"]) ? $_FILES['document']["tmp_name"] : null);
+    $docfileName                = (isset($_FILES['document']["name"]) ? $_FILES['document']["name"] : null);
+    $document_comment           = (isset($_POST['document_comment']) ? $_POST['document_comment'] : null);
+    $creator                    = (isset($_POST['creator']) ? $_POST['creator'] : null);
+    $creator_comment            = (isset($_POST['creator_comment']) ? $_POST['creator_comment'] : null);
+    $pakage_version             = (isset($_POST['pacakge_version']) ? $_POST['pacakge_version'] : null);
+    $package_supplier           = (isset($_POST['package_supplier']) ? $_POST['package_supplier'] : null);
+    $package_originator         = (isset($_POST['package_originator']) ? $_POST['package_originator'] : null);
+    $package_download_location  = (isset($_POST['package_download_location']) ? $_POST['package_download_location'] : null);
+    $package_home_page          = (isset($_POST['package_home_page']) ? $_POST['package_home_page'] : null);
+    $package_source_info        = (isset($_POST['package_source_info']) ? $_POST['package_source_info'] : null);
+    $package_license_comments   = (isset($_POST['package_license_comments']) ? $_POST['package_license_comments'] : null);
+    $package_description        = (isset($_POST['package_description']) ? $_POST['package_description'] : null);
     $scan_option 				= (isset($_POST['scan_option']) ? $_POST['scan_option'] : null);
+    
 
-    move_uploaded_file($filePath,"uploads/$fileName");
-    while (!file_exists("uploads/$fileName")) sleep(1);
     
-    $commandLine = "$DoSOCS --scan --packagePath \"uploads/$fileName\"";
-    if(!empty($document_comment)) {
-        $commandLine .= " --documentComment \"$document_comment\"";
-    }
-    if(!empty($creator)) {
-        $commandLine .= " --creator \"$creator\"";
-    }
-    if(!empty($creator_comment)) {
-        $commandLine .= " --creatorComment \"$creator_comment\"";
-    }
-    if(!empty($pakage_version)) {
-        $commandLine .= " --packageVersion \"$pakage_version\"";
-    }
-    if(!empty($package_supplier)) {
-        $commandLine .= " --packageSupplier \"$package_supplier\"";
-    }
-    if(!empty($package_originator)) {
-        $commandLine .= " --packageOriginator \"$package_originator\"";
-    }
-    if(!empty($package_download_location)) {
-        $commandLine .= " --packageDownloadLocation \"$package_download_location\"";
-    }
-    if(!empty($package_home_page)) {
-        $commandLine .= " --packageHomePage \"$package_home_page\"";
-    }
-    if(!empty($package_source_info)) {
-        $commandLine .= " --packageSourceInfo \"$package_source_info\"";
-    }
-    if(!empty($package_license_comments)) {
-        $commandLine .= " --packageLicenseComments \"$package_license_comments\"";
-    }
-    if(!empty($package_description)) {
-        $commandLine .= " --documentComment \"$package_description\"";
+    if(!empty($packagefilePath)){
+        move_uploaded_file($packagefilePath,"uploads/$packagefileName");
+    	while (!file_exists("uploads/$packagefileName")) sleep(1);
+    
+	    $commandLine = "$DoSOCS --scan --packagePath \"uploads/$packagefileName\"";
+	    if(!empty($document_comment)) {
+	        $commandLine .= " --documentComment \"$document_comment\"";
+	    }
+	    if(!empty($creator)) {
+	        $commandLine .= " --creator \"$creator\"";
+	    }
+	    if(!empty($creator_comment)) {
+	        $commandLine .= " --creatorComment \"$creator_comment\"";
+	    }
+	    if(!empty($pakage_version)) {
+	        $commandLine .= " --packageVersion \"$pakage_version\"";
+	    }
+	    if(!empty($package_supplier)) {
+	        $commandLine .= " --packageSupplier \"$package_supplier\"";
+	    }
+	    if(!empty($package_originator)) {
+	        $commandLine .= " --packageOriginator \"$package_originator\"";
+	    }
+	    if(!empty($package_download_location)) {
+	        $commandLine .= " --packageDownloadLocation \"$package_download_location\"";
+	    }
+	    if(!empty($package_home_page)) {
+	        $commandLine .= " --packageHomePage \"$package_home_page\"";
+	    }
+	    if(!empty($package_source_info)) {
+	        $commandLine .= " --packageSourceInfo \"$package_source_info\"";
+	    }
+	    if(!empty($package_license_comments)) {
+	        $commandLine .= " --packageLicenseComments \"$package_license_comments\"";
+	    }
+	    if(!empty($package_description)) {
+	        $commandLine .= " --documentComment \"$package_description\"";
+	    }
+	    
+	    if(!empty($scan_option)) {
+	        $commandLine .= " --scanOption \"$scan_option\"";
+	        shell_exec($commandLine . ' 2>&1');
+	    	echo "<div align=\"center\">Document successfully uploaded.</div>";
+	    }
+	    else{
+	    	echo "<div align=\"center\">Error processing request.</div>";
+	    }
     }
     
-    if(!empty($scan_option)) {
-        $commandLine .= " --scanOption \"$scan_option\"";
-        shell_exec($commandLine . ' 2>&1');
-    	echo "<div align=\"center\">Document successfully uploaded.</div>";
-    }
-    else{
-    	echo "<div align=\"center\">Error processing request.</div>";
-    }
+   	if(!empty($docfilePath)){
+   	    move_uploaded_file($docfilePath,"uploads/$docfileName");
+    	while (!file_exists("uploads/$docfileName")) sleep(1);
+   	}
    
-	incFooter();
+	include("inc/_footer.php");
 ?>
