2c481c91c02a9fdc3da4b11efaa8a43720f7cef6
Jacob Overkamp <jmoverkamp@gmail.com>
2015-03-04 02:02:27 -0600

    Trivial regex for file parsing. Will need to expand further before creating SQL queries.

--- src/upload_action.php
+++ src/upload_action.php
@@ -27,7 +27,7 @@ limitations under the License.
     $document_comment           = (isset($_POST['document_comment']) ? $_POST['document_comment'] : null);
     $creator                    = (isset($_POST['creator']) ? $_POST['creator'] : null);
     $creator_comment            = (isset($_POST['creator_comment']) ? $_POST['creator_comment'] : null);
-    $pakage_version             = (isset($_POST['pacakge_version']) ? $_POST['pacakge_version'] : null);
+    $pakage_version             = (isset($_POST['package_version']) ? $_POST['package_version'] : null);
     $package_supplier           = (isset($_POST['package_supplier']) ? $_POST['package_supplier'] : null);
     $package_originator         = (isset($_POST['package_originator']) ? $_POST['package_originator'] : null);
     $package_download_location  = (isset($_POST['package_download_location']) ? $_POST['package_download_location'] : null);
