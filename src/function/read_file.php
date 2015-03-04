17c28a838105a2e729b63a074355e3d121d6e348
Jacob Overkamp <jmoverkamp@gmail.com>
2015-03-04 13:21:12 -0600

    Added ability to scan a single file from a package to make document uploadable to DB. Will revise at a later time.

--- src/function/read_file.php
+++ src/function/read_file.php
@@ -49,8 +49,29 @@
 		$create = "";
 		$updated = "";
 		
+		//FILES
+		$file_name = "";
+		$file_type = "";
+		$file_copyright_text = "";
+		$artifact_of_project_name = "";
+		$artifact_of_project_homepage = "";
+		$artifact_of_project_uri = "";
+		$f_license_concluded = "";
+		$f_license_info_in_file = "";
+		$file_checksum = "";
+		$file_checksum_algorithm = "";
+		$f_relative_path = "";
+		$f_license_comments = "";
+		$file_notice = "";
+		$file_contributor = "";
+		$file_dependency = "";
+		$file_comment = "";
+		$f_created_at = "";
+		$f_updated_at = "";
+		
 		$docID;
 		$packageID;
+		#fileID;
 		
 		// CREATOR
 		$c_generated_at = NULL;
@@ -141,6 +162,49 @@
 		}
 		$updated = NULL;
 		
+		
+		//FILES
+		if (preg_match("/<spdx:referencesFile>.*<spdx:fileName>(?P<name>.*?)<\/spdx:fileName>/", $myString, $matches)) {
+		  	$file_name = $matches[1] ?: NULL;
+		}
+		if (preg_match("/<spdx:referencesFile>.*<spdx:fileType rdf:resource=\"http:\/\/spdx.org\/rdf\/terms(?P<name>.*?)\"\/>/", $myString, $matches)) {
+		  	$file_type = $matches[1] ?: NULL;
+		}
+		if (preg_match("/<spdx:referencesFile>.*<spdx:copyrightText>(?P<name>.*?)<\/spdx:copyrightText>/", $myString, $matches)) {
+		  	$file_copyright_text = $matches[1] ?: NULL;
+		}
+		$artifact_of_project_name= NULL;
+		$artifact_of_project_homepage = NULL;
+		$artifact_of_project_uri = NULL;
+		if (preg_match("/<spdx:referencesFile>.*<spdx:licenseConcluded rdf:resource=\"http:\/\/spdx.org\/licenses\/(?P<name>.*?)\"\/>/", $myString, $matches)) {
+		  	$f_license_concluded = $matches[1] ?: NULL;
+		}
+		if (preg_match("/<spdx:referencesFile>.*<spdx:licenseInfoInFile rdf:resource=\"http:\/\/spdx.org\/licenses\/(?P<name>.*?)\"\/>/", $myString, $matches)) {
+		  	$f_license_info_in_file = $matches[1] ?: NULL;
+		}
+		
+		if (preg_match("/<spdx:referencesFile>.*<spdx:checksumValue>(?P<name>.*?)<\/spdx:checksumValue>/", $myString, $matches)) {
+		  	$file_checksum = $matches[1] ?: NULL;
+		}
+		if (preg_match("/<spdx:referencesFile>.*<spdx:algorithm rdf:resource=\"http:\/\/spdx.org\/rdf\/terms#checksumAlgorithm_(?P<name>.*?)\"\/>/", $myString, $matches)) {
+		  	$file_checksum_algorithm = $matches[1] ?: NULL;
+		}
+		$f_relative_path = NULL;
+		if (preg_match("/<spdx:referencesFile>.*<spdx:licenseComments>(?P<name>.*?)<\/spdx:licenseComments>/", $myString, $matches)) {
+		  	$f_license_comments = $matches[1] ?: NULL;
+		}
+		$file_notice = NULL;
+		$file_contributor = NULL;
+		$file_dependency = NULL;
+		$file_comment = NULL;
+		if (preg_match("/<spdx:created>(?P<name>.*?)<\/spdx:created>/", $myString, $matches)) {
+		  	$f_created_at = $matches[1] ?: NULL;
+		}
+		$f_updated_at = NULL;
+		
+		
+		
+		
         mysql_connect("$host", "$username", "$password")or die("cannot connect server " . mysql_error());
         mysql_select_db("$db_name")or die("cannot select DB " . mysql_error());
         
@@ -192,18 +256,37 @@
    		 	echo "Error: " . mysql_error();
 		}
 		
+		
+        //Query
+        $sql  = "INSERT INTO `package_files` (`file_name`,`file_type`,`file_copyright_text`,`artifact_of_project_name`,
+        		`artifact_of_project_homepage`,`artifact_of_project_uri`,`license_concluded`,`license_info_in_file`,`file_checksum`,
+        		`file_checksum_algorithm`,`relative_path`,`license_comments`,`file_notice`,`file_contributor`,`file_dependency`,
+        		`file_comment`,`created_at`,`updated_at`) 
+        		VALUES ('$file_name', '$file_type', '$file_copyright_text', '$artifact_of_project_name', '$artifact_of_project_homepage',
+				'$artifact_of_project_uri', '$f_license_concluded', '$f_license_info_in_file', '$file_checksum', '$file_checksum_algorithm',
+				'$f_relative_path', '$f_license_comments', '$file_notice', '$file_contributor', '$file_dependency', '$file_comment',
+				'$f_created_at', '$f_updated_at')";
+        
+        //Execute Query
+		if (mysql_query($sql)){
+    		echo "New record created successfully";
+    		$fileID = mysql_insert_id();
+		} else {
+   		 	echo "Error: " . mysql_error();
+   		 	$fileID = NULL;
+		}
+		
+
 		//Query
         $sql  = "INSERT INTO `doc_file_package_associations` (`spdx_doc_id`,`package_id`,`package_file_id`,`created_at`,`updated_at`) 
-        		VALUES ('$docID', '$packageID', 1, NOW(), NULL)";
+        		VALUES ('$docID', '$packageID', '$fileID', NOW(), NULL)";
         
         //Execute Query
 		if (mysql_query($sql)){
     		echo "New record created successfully";
 		} else {
    		 	echo "Error: " . mysql_error();
-		}
-		
-		
+		}		
         //Close Connection
         mysql_close();
     }
