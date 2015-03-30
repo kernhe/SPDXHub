8486921142545847b910f62a8f29717b93812e93
Jacob Overkamp <jmoverkamp@gmail.com>
2015-03-30 10:42:47 -0500

    Updated get_package to match rdf in 2.0 spec..

--- src/function/get/get_package.php
+++ src/function/get/get_package.php
@@ -7,49 +7,55 @@
 		}		
 		
     	$packageArray = array(
-			$p_name = $docFile ?: NULL,
-			$p_file_name = $docFile ?: NULL,
-			$p_download_location = $filePath ?: NULL,
-			$p_copyright_text = "",
-			$p_version = "",
-			$p_description = "",
-			$p_summary = "",
-			$p_originator = "",
-			$p_supplier = "",
-			$p_license_concluded = "",
-			$p_license_declared = "",
-			$p_checksum = "",
-			$check_algorithm = "",
-			$p_home_page = "",
-			$p_source_info = "",
-			$p_license_comments = "",
-			$p_verification_code = "",
-			$p_verification_code_excluded_file = "",
-			$create = "",
-			$updated = "",
+			$name = $docFile ?: NULL,
+			$pspdx_id = "",
+			$version = "",
+			$filename = "",
+			$supplier = "",
+			$originator = "",
+			$download_location = "",
+			$checksum = "",
+			$verificationcode = "",
+			$home_page = "",
+			$source_Information = "",
+			$source_info = "",
+			$license_declared = "",
+			$license_concluded = "",
+			$license_info_from_files = "",
+			$license_comment = "",
+			$package_copyright_text = "",
+			$summary = "",
+			$description = "",
+			$summary_description = "",
+			$package_detailed_description = "",
+			$package_comment = "",
 		);
 		
 		$rdf_regex = array(
-			$p_name = NULL,
-			$p_file_name = "<spdx:packageFileName>(?P<name>.*?)<\/spdx:packageFileName>",
-			$p_download_location = NULL,
-			$p_copyright_text = "<spdx:copyrightText>(?P<name>.*?)<\/spdx:copyrightText>",
-			$p_version = "<spdx:versionInfo>(?P<name>.*?)<\/spdx:versionInfo>",
-			$p_description = "<spdx:description>(?P<name>.*?)<\/spdx:description>",
-			$p_summary = "<spdx:summary>(?P<name>.*?)<\/spdx:summary>",
-			$p_originator = "<spdx:originator>(?P<name>.*?)<\/spdx:originator>",
-			$p_supplier = "<spdx:supplier>(?P<name>.*?)<\/spdx:supplier>",
-			$p_license_concluded = "<spdx:licenseConcluded rdf:resource=\"http:\/\/spdx.org\/licenses\/(?P<name>.*?)\"\/>",
-			$p_license_declared = "<spdx:licenseDeclared rdf:resource=\"http:\/\/spdx.org\/licenses\/(?P<name>.*?)\"\/>",
-			$p_checksum = "<spdx:checksum rdf:nodeID=\"(?P<name>.*?)\"\/>",
-			$check_algorithm = NULL,
-			$p_home_page = "<doap:homepage>(?P<name>.*?)<\/doap:homepage>",
-			$p_source_info = NULL,
-			$p_license_comments = "<spdx:licenseComments>(?P<name>.*?)<\/spdx:licenseComments>",
-			$p_verification_code = "<spdx:verificationCodeValue>(?P<name>.*?)<\/spdx:verificationCodeValue>",
-			$p_verification_code_excluded_file = "<packageVerificationCodeExcludedFile>(?P<name>.*?)<\/packageVerificationCodeExcludedFile>",
-			$created = "<spdx:created>(?P<name>.*?)<\/spdx:created>",
-			$updated = NULL,
+			$name = "<spdx:packageFileName>(?P<name>.*?)<\/spdx:packageFileName>",
+			$pspdx_id = NULL,
+			$version = "<spdx:versionInfo>(?P<name>.*?)<\/spdx:versionInfo>",
+			$filename = NULL,
+			$supplier = "<spdx:supplier>(?P<name>.*?)<\/spdx:supplier>",
+			$originator = "<spdx:originator>(?P<name>.*?)<\/spdx:originator>",
+			$download_location = NULL,
+			$checksum = "<spdx:checksum rdf:nodeID=\"(?P<name>.*?)\"\/>",
+			$verificationcode = "<spdx:verificationCodeValue>(?P<name>.*?)<\/spdx:verificationCodeValue>",
+			$home_page = "<doap:homepage>(?P<name>.*?)<\/doap:homepage>",
+			$source_Information = NULL,
+			$source_info = NULL,
+			$license_declared = "<spdx:licenseDeclared rdf:resource=\"http:\/\/spdx.org\/licenses\/(?P<name>.*?)\"\/>",
+			$license_concluded = "<spdx:licenseConcluded rdf:resource=\"http:\/\/spdx.org\/licenses\/(?P<name>.*?)\"\/>",
+			$license_info_from_files = NULL,
+			$license_comment = "<spdx:licenseComments>(?P<name>.*?)<\/spdx:licenseComments>",
+			$package_copyright_text = "<spdx:copyrightText>(?P<name>.*?)<\/spdx:copyrightText>",
+			$summary = "<spdx:summary>(?P<name>.*?)<\/spdx:summary>",
+			$description = "<spdx:description>(?P<name>.*?)<\/spdx:description>",
+			$summary_description = NULL,
+			$package_detailed_description = NULL,
+			$package_comment = NULL,
+			
+			
 		);
 		
 		$regex = array(
@@ -65,15 +71,16 @@
 			}
 		}
 		
-		$query 	= 	"INSERT INTO `packages` (`package_name`, `package_file_name`, `package_download_location`, `package_copyright_text`, 
-	        		`package_version`, `package_description`, `package_summary`, `package_originator`, `package_supplier`,
-					`package_license_concluded`, `package_license_declared`, `package_checksum`, `checksum_algorithm`,
-					`package_home_page`, `package_source_info`, `package_license_comments`, `package_verification_code`,
-					`package_verification_code_excluded_file`, `created_at`, `updated_at`) 
+		$query 	= 	"INSERT INTO `spdx_package_info` (`name`, `pspdx_id`, `version`, `filename`, 
+	        		`supplier`, `originator`, `download_location`, `checksum`, `verificationcode`,
+					`home_page`, `source_Information`, `source_info`, `license_declared`,
+					`license_concluded`, `license_info_from_files`, `license_comment`, `package_copyright_text`,
+					`summary`, `description`, `summary_description`, `package_detailed_description`,
+					`package_comment`) 
 					VALUES ('$packageArray[0]', '$packageArray[1]', '$packageArray[2]', '$packageArray[3]', '$packageArray[4]', '$packageArray[5]',
 					'$packageArray[6]', '$packageArray[7]', '$packageArray[8]', '$packageArray[9]', '$packageArray[10]', '$packageArray[11]',
 					'$packageArray[12]', '$packageArray[13]', '$packageArray[14]', '$packageArray[15]', '$packageArray[16]',
-					'$packageArray[17]', '$packageArray[18]', '$packageArray[19]')";
+					'$packageArray[17]', '$packageArray[18]', '$packageArray[19]', $packageArray[20]', $packageArray[21]')";
 					
 		return $query;
     }
