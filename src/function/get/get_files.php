92e6148b730da83ee5747275ba52824d131fa490
Jacob Overkamp <jmoverkamp@gmail.com>
2015-03-30 12:00:15 -0500

    Updated get_files.php to involve use of 2.0 spec.

--- src/function/get/get_files.php
+++ src/function/get/get_files.php
@@ -7,45 +7,37 @@
 		}		
 
 		$fileArray = array(
-			$file_name = "",
-			$file_type = "",
+			$filename = "",
+			$filetype = "",
+			$checksum = "",
+			$license_concluded = "",
+			$license_info_in_file = "",
+			$license_comment = "",
 			$file_copyright_text = "",
-			$artifact_of_project_name = "",
-			$artifact_of_project_homepage = "",
-			$artifact_of_project_uri = "",
-			$f_license_concluded = "",
-			$f_license_info_in_file = "",
-			$file_checksum = "",
-			$file_checksum_algorithm = "",
-			$f_relative_path = "",
-			$f_license_comments = "",
+			$artifact_of_project = "",
+			$artifact_of_homepage = "",
+			$artifact_of_url = "",
+			$file_comment = "",
 			$file_notice = "",
 			$file_contributor = "",
-			$file_dependency = "",
-			$file_comment = "",
-			$f_created_at = "",
-			$f_updated_at = "",
+			$package_info_fk = "",
+			$spdx_fk = "",
 		);
 		
 		$rdf_regex = array(
-			$file_name = "<spdx:referencesFile>.*<spdx:fileName>(?P<name>.*?)<\/spdx:fileName>",
-			$file_type = "<spdx:referencesFile>.*<spdx:fileType rdf:resource=\"http:\/\/spdx.org\/rdf\/terms(?P<name>.*?)\"\/>",
+			$filename = "<spdx:referencesFile>.*<spdx:fileName>(?P<name>.*?)<\/spdx:fileName>",
+			$filetype = "<spdx:referencesFile>.*<spdx:fileType rdf:resource=\"http:\/\/spdx.org\/rdf\/terms(?P<name>.*?)\"\/>",
+			$checksum = "<spdx:referencesFile>.*<spdx:checksumValue>(?P<name>.*?)<\/spdx:checksumValue>",
+			$license_concluded = "<spdx:referencesFile>.*<spdx:licenseConcluded rdf:resource=\"http:\/\/spdx.org\/licenses\/(?P<name>.*?)\"\/>",
+			$license_info_in_file = "<spdx:referencesFile>.*<spdx:licenseInfoInFile rdf:resource=\"http:\/\/spdx.org\/licenses\/(?P<name>.*?)\"\/>",
+			$license_comment = "<spdx:referencesFile>.*<spdx:licenseComments>(?P<name>.*?)<\/spdx:licenseComments>",
 			$file_copyright_text = "<spdx:referencesFile>.*<spdx:copyrightText>(?P<name>.*?)<\/spdx:copyrightText>",
-			$artifact_of_project_name = NULL,
-			$artifact_of_project_homepage = NULL,
-			$artifact_of_project_uri = NULL,
-			$f_license_concluded = "<spdx:referencesFile>.*<spdx:licenseConcluded rdf:resource=\"http:\/\/spdx.org\/licenses\/(?P<name>.*?)\"\/>",
-			$f_license_info_in_file = "<spdx:referencesFile>.*<spdx:licenseInfoInFile rdf:resource=\"http:\/\/spdx.org\/licenses\/(?P<name>.*?)\"\/>",
-			$file_checksum = "<spdx:referencesFile>.*<spdx:checksumValue>(?P<name>.*?)<\/spdx:checksumValue>",
-			$file_checksum_algorithm = "<spdx:referencesFile>.*<spdx:algorithm rdf:resource=\"http:\/\/spdx.org\/rdf\/terms#checksumAlgorithm_(?P<name>.*?)\"\/>",
-			$f_relative_path = NULL,
-			$f_license_comments = "<spdx:referencesFile>.*<spdx:licenseComments>(?P<name>.*?)<\/spdx:licenseComments>",
-			$file_notice = NULL,
-			$file_contributor = NULL,
-			$file_dependency = NULL,
+			$artifact_of_project = NULL,
+			$artifact_of_homepage = NULL,
+			$artifact_of_url = NULL,
 			$file_comment = NULL,
-			$created = "<spdx:created>(?P<name>.*?)<\/spdx:created>",
-			$f_updated_at = NULL,
+			$file_notice = NULL,
+			$file_contributor = NULL,	
 		);
 		
 		$regex = array(
@@ -61,14 +53,12 @@
 			}
 		}
 		
-        $query	= 	"INSERT INTO `package_files` (`file_name`,`file_type`,`file_copyright_text`,`artifact_of_project_name`,
-	        		`artifact_of_project_homepage`,`artifact_of_project_uri`,`license_concluded`,`license_info_in_file`,`file_checksum`,
-	        		`file_checksum_algorithm`,`relative_path`,`license_comments`,`file_notice`,`file_contributor`,`file_dependency`,
-	        		`file_comment`,`created_at`,`updated_at`) 
+        $query	= 	"INSERT INTO `spdx_file_info` (`filename`,`filetype`,`checksum`,`license_concluded`,
+	        		`license_info_in_file`,`license_comment`,`file_copyright_text`,`artifact_of_project`,`artifact_of_homepage`,
+	        		`artifact_of_url`,`file_comment`,`file_notice`,`file_contributor`,`package_info_fk`,`spdx_fk`) 
 	        		VALUES ('$fileArray[0]', '$fileArray[1]', '$fileArray[2]', '$fileArray[3]', '$fileArray[4]',
 					'$fileArray[5]', '$fileArray[6]', '$fileArray[7]', '$fileArray[8]', '$fileArray[9]',
-					'$fileArray[10]', '$fileArray[11]', '$fileArray[12]', '$fileArray[13]', '$fileArray[14]', '$fileArray[15]',
-					'$fileArray[16]', '$fileArray[17]')";
+					'$fileArray[10]', '$fileArray[11]', '$fileArray[12]', '$fileArray[13]', '$fileArray[14]')";
 					
 		return $query;
     }
