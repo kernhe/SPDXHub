94b981acef65b58f4df6c864eb646f6814e043fa
nchickinelli <nchickinelli@gmail.com>
2015-04-01 18:17:21 -0500

    structure html

--- src/index.php
+++ src/index.php
@@ -10,26 +10,10 @@
 ?>
 			
 <div class="container" >
-  <div class="col-xs-12">
-    <h1 id="moreOptions"><img src="images/arrow-closed.png" />More Options</h1>
-    
-    <div class="col-xs-12" id="toggleTable">
-      <label for="uri-charset"><strong>Licences</strong></label>
 
-      <input id="" name="outline" type="checkbox" value="1" />
-      <label title="SPDX approved" for="">SPDX approved</label>
-
-      <input id="" name="No200" type="checkbox" value="1" />
-      <label title="SPDX Not Approved" for="">SPDX Not Approved</label>
-
-      <input id="uri-verbose2" name="" type="checkbox" value="1" />
-      <label title="Not in SPDX list" for="">Not in SPDX list</label>
-    </div>
-  </div>
-  
   <div class="col-xs-12">
     
-    <h2>License name</h2>
+    <h3>License name</h3>
     
     <select class="LicenseListDropDown" name="charset">
       <option value="(license identifier)" selected="selected">(license names)</option>
@@ -47,69 +31,80 @@
     <div id="identifier">Identifier</div>
   </div>
 
+  <div class="col-xs-12">
+    
+    <h3 id="moreOptions"><!-- <img src="images/arrow-closed.png" /> -->Advanced Search</h3>
+    
+    <div id="toggleTable">
+      <label for="uri-charset"><strong>Licences</strong></label>
+
+      <input id="" name="outline" type="checkbox" value="1" />
+      <label title="SPDX approved" for="">SPDX approved</label>
+
+      <input id="" name="No200" type="checkbox" value="1" />
+      <label title="SPDX Not Approved" for="">SPDX Not Approved</label>
+
+      <input id="uri-verbose2" name="" type="checkbox" value="1" />
+      <label title="Not in SPDX list" for="">Not in SPDX list</label>
+    </div>
+  </div>
+
 </div>
         
 <div id="container">
- 			 <table id = "mytablesorter" class="display">
-            
-            <thead>
-              <tr>
-              	<th></th>
-                <th style="width:200px"></th>
-                <th style="width:150px">Created on</th>
-                <th style="width:150px">Last updated</th>
-                <th>&nbsp;</th>
-                <th>&nbsp;</th>
-              </tr>
-              <tr>
-              	<th>#</th>
-                <th>Document Name</th>
-                <th>Created on</th>
-                <th>Last updated</th>
-                <th>Licences</th>
-                <th>Action</th>
-              </tr>
-            </thead>  
+ 	<div class="col-xs-12">	 
+       
+    <table id="mytablesorter" class=" table display">
+      
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Document Name</th>
+          <th>Created on</th>
+          <th>Last updated</th>
+          <th>Licences</th>
+          <th>Action</th>
+        </tr>
+      </thead>  
 
-            <tbody>
-              <?php
-                $count = 0;
-				
-				$result = getSPDX_DocList($name);
-				
-                while($row = mysql_fetch_assoc($result)) {
-                    echo '<tr>';
-					echo     '<td>';
-                    echo       $count+1;
-                    echo     '</td>';
-                    echo     '<td>';
-                    echo         '<a href="spdx_doc.php?doc_id=' . $row['spdx_pk'] . '">' . $row['document_name'] . '</a>';
-                    echo     '</td>';
-                    echo     '<td>';
-                    echo         date('m/d/Y', strtotime($row['created_date'])); 
-                    echo     '</td>';
-					echo     '<td>';
-                    echo         date('m/d/Y', strtotime($row['created_date'])); 
-                    echo     '</td>';
-			        echo     '<td>';
-                    echo         '<img src="../src/images/flags.jpg" width="83" height="26" />';
-                    echo     '</td>';
-                    echo     '<td>';
-                    echo         '<div>';
-                    echo             '<button type="button" class="btn" onclick="window.location=\'spdx_doc.php?doc_id=' . $row['spdx_pk'] . '\'">View Details</button>';
-                    echo             '<button type="button" class="btn">Download</button>'; 
-                    echo         '</div>';
-                    echo     '</td>';
-                    echo '</tr>';
+      <tbody>
+        <?php
+          $count = 0;
+		
+		      $result = getSPDX_DocList($name);
+		
+          while($row = mysql_fetch_assoc($result)) {
+            echo '<tr>';
+	          echo     '<td>';
+            echo       $count+1;
+            echo     '</td>';
+            echo     '<td>';
+            echo         '<a href="spdx_doc.php?doc_id=' . $row['spdx_pk'] . '">' . $row['document_name'] . '</a>';
+            echo     '</td>';
+            echo     '<td>';
+            echo         date('m/d/Y', strtotime($row['created_date'])); 
+            echo     '</td>';
+	          echo     '<td>';
+            echo         date('m/d/Y', strtotime($row['created_date'])); 
+            echo     '</td>';
+            echo     '<td>';
+            echo         '<img src="../src/images/flags.jpg" width="83" height="26" />';
+            echo     '</td>';
+            echo     '<td>';
+            echo         '<div>';
+            echo             '<button type="button" class="btn" onclick="window.location=\'spdx_doc.php?doc_id=' . $row['spdx_pk'] . '\'">View Details</button>';
+            echo             '<button type="button" class="btn">Download</button>'; 
+            echo         '</div>';
+            echo     '</td>';
+            echo '</tr>';
 
-                    $count++;
-                }
-                ?>
-            </tbody>
-          </table>
+            $count++;
+          }
+        ?>
+      </tbody>
 
-
-
+    </table>
+  </div>     
 </div>
  
 
