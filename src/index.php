145122a254ba447b73f96ea0deaf9198973ac183
nchickinelli <nchickinelli@gmail.com>
2015-04-04 17:04:07 -0500

    html and style

--- src/index.php
+++ src/index.php
@@ -1,74 +1,81 @@
 
-
-<div class="container" >
-
-  <div class="col-xs-12 blue-bord" style="height: 50px">
 <?php
-    $title = "SPDX";
-    include("function/_header.php");
-    include("function/spdx_doc.php");
-    $name = "";
-    if(array_key_exists('doc_name',$_POST)) {
-    	$name = $_POST['doc_name'];
-    }
+  $title = "SPDX";
+  include("function/_header.php");
+  include("function/spdx_doc.php");
+  $name = "";
+  if(array_key_exists('doc_name',$_POST)) {
+    $name = $_POST['doc_name'];
+  }
 ?>
-    
-  </div>
 
-
-  <div class="col-xs-12 col-md-6 red-bord">
-       <form action="index.php" method="post" >
-    		<input type="text" class="" size="60" tabindex="2" autofocus="autofocus" placeholder="Search" value="<?php 		echo $name; ?>" name="doc_name"/>
-    		<button type="submit" class="btn pull-right">Search</button>
-    	</form>
-  </div>
+<div class="container search">
   
-  <div class="col-xs-12 blue-bord">
+  <div class="col-xs-12">
     <h4 class="search-header">Advanced Search</h4>
   </div>
 
-  <div class="col-xs-12 col-md-6 red-bord">   
-    <h6>Identifier</strong></h6>
-    <div id="identifier">Identifier</div> 
-    
-    <select class="LicenseListDropDown" name="charset">
-      <option value="(license identifier)" selected="selected">license name</option>
-      <?php
-      $resultlist = getSPDX_LicenseList();
-        while($row = mysql_fetch_assoc($resultlist)) {
-          echo '<option value="' . $row['license_identifier'] . '">';
-          echo         $row['license_fullname'];
-          echo '</option>';
-         }
-      ?>
-    </select>                
-
-    <div><strong>Identifier</strong></div>
-    <div id="identifier">Identifier</div> 
+  <div class="col-xs-12 search-inner">
+    <form class="" action="index.php" method="post" >
+    	<span class="col-xs-10 col-sm-8 col-md-6"><!-- autofocus="autofocus" -->
+        <input type="search" class="form-control" placeholder="Search" value="<?php echo $name; ?>" name="doc_name"/>
+        <button type="submit" class="btn pull-right">Search</button>
+      </span>
+  	</form>
   </div>
 
-
-  <div class="col-xs-12 col-md-6  red-bord">
-    <form>
-      <h5>Licences</h5>
-      <!-- <label for="uri-charset"><strong>Licences</strong></label> -->
-
-      <input id="" name="outline" type="checkbox" value="1" />
-      <label title="SPDX approved" for="">SPDX approved</label>
-
-      <input id="" name="No200" type="checkbox" value="1" />
-      <label title="SPDX Not Approved" for="">SPDX Not Approved</label>
-
-      <input id="uri-verbose2" name="" type="checkbox" value="1" />
-      <label title="Not in SPDX list" for="">Not in SPDX list</label>
-    </form>
-  </div>
- 
 </div>
 
-<div class="container">
-  <div class="col-xs-12">  
+<div class="container adv-search"> 
     
+  <div class="col-xs-12">
+   <!--  <form>
+      <input type="text" />
+      <button type="submit">Search</button>
+    </form> -->
+
+    <h4 class="search-header">Advanced Search</h4>
+  </div>
+  
+  <div class="col-xs-12 adv-search-inner">
+    
+    <div class="col-xs-12 col-md-6  ">
+      <h5>Licences</h5>
+      <form>
+        <ul class="license-filter">
+          <li>
+            <input id="" name="outline" type="checkbox" value="1" />
+            <label title="SPDX approved" for="">SPDX approved</label>
+          </li>
+          <li>
+            <input id="" name="No200" type="checkbox" value="1" />
+            <label title="SPDX Not Approved" for="">SPDX Not Approved</label>
+          </li>
+          <li>
+            <input id="uri-verbose2" name="" type="checkbox" value="1" />
+            <label title="Not in SPDX list" for="">Not in SPDX list</label>
+          </li>
+        </ul>
+      </form>
+    </div>
+
+    <div class="col-xs-12 col-md-6">   
+      
+      <h5>Identifier</strong></h><span id="identifier">Identifier</span> 
+      
+      <select class="LicenseListDropDown" name="charset">
+        <option value="(license identifier)" selected="selected">license name</option>
+        <?php
+        $resultlist = getSPDX_LicenseList();
+          while($row = mysql_fetch_assoc($resultlist)) {
+            echo '<option value="' . $row['license_identifier'] . '">';
+            echo $row['license_fullname'];
+            echo '</option>';
+           }
+        ?>
+      </select>                
+    </div>
+
   </div>
 </div>
         
@@ -76,18 +83,18 @@
  	<div class="col-xs-12">	 
        
     <table id="tablesorter" class="table table-striped table-hover display">
+        
         <thead> 
           <tr>
             <th>#</th>
             <th>Document Name</th>
             <th>Created on</th>
-            <!-- <th>Last updated</th> -->
             <th>Licences</th>
             <th>Action</th>
           </tr>
         </thead>
   
-        <tbody class="" >
+        <tbody>
           <?php
             
             $result = getSPDX_DocList($name);
@@ -101,15 +108,21 @@
                 echo     '</td>';
                 echo     '<td>';
                 echo         date('m/d/Y', strtotime($row['created_date'])); 
-                echo     '</td>';
-		            echo     '<td>';
-		            echo         '<img src="../src/images/flags.jpg" width="83" height="26" />';
-		            echo     '</td>';
-                echo     '<td style="text-align:right;">';
+                echo     '</td>'; ?>
+		            <!-- '<img src="../src/images/flags.jpg" width="83" height="26" />';  -->
+                  <td id="breakdown">
+                    <div>
+                      <span class="b-one"></span>
+                      <span class="b-two"></span>
+                      <span class="b-three"></span>
+                    </div>
+                  </td>
+                <?php
+                echo     '<td id="action">';
                 echo         '<div>';
-                echo             '<button type="button" class="btn" onclick="window.open(\'download.php?doc_id=' . $row['spdx_pk'] . '&format=RDF&doc_name=' . $row['document_name'] . '\',\'_blank\');">Download RDF</button>';
-                echo             '<button type="button" class="btn" onclick="window.open(\'download.php?doc_id=' . $row['spdx_pk'] . '&format=TAG&doc_name=' . $row['document_name'] . '\',\'_blank\');">Download TAG</button>';
-                echo             '<button type="button" class="btn" onclick="window.open(\'download.php?doc_id=' . $row['spdx_pk'] . '&format=JSON&doc_name=' . $row['document_name'] . '\',\'_blank\');">Download JSON</button>';
+                echo             '<button type="button" class="btn btn-info" onclick="window.location=\'spdx_doc.php?doc_id=' . $row['id'] . '\'">View Details</button>';
+                echo             '<button type="button" class="btn btn-default" onclick="window.open(\'download.php?doc_id=' . $row['spdx_pk'] . '&format=RDF&doc_name=' . $row['document_name'] . '\',\'_blank\');">Download RDF</button>';
+                echo             '<button type="button" class="btn btn-default" onclick="window.open(\'.php?doc_id=' . $row['spdx_pk'] . '&format=TAG&doc_name=' . $row['document_name'] . '\',\'_blank\');">Download TAG</button>';
                 echo         '</div>';
                 echo     '</td>';
                 echo '</tr>';
