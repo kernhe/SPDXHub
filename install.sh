19b28bae2fdecd9d7bbb63808beadc53718233cd
Jacob Overkamp <jmoverkamp@gmail.com>
2015-04-04 23:44:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,28 @@
 u=$USER
 p=$PWD
 
+#Install dependencies, may remove later
+sudo apt-get install apache2
+sudo apt-get install mysql-server
+sudo apt-get install php5 libapache2-mod-php5
+sudo apt-get install php5-mysql
+sudo apt-get install git
+
 #Clone Repos
-git clone https://github.com/socs-dev-env/SOCSDatabase
+echo "Moving to /var/www/ ..."
+cd /var/www/
 git clone https://github.com/socs-dev-env/DoSOCS
 git clone https://github.com/jmoverkamp/SPDXHub
 
+#Config apache
+sudo cp /var/www/SPDXHub/doc/SPDXHub.conf /etc/apache2/sites-available/SPDXHub.conf
+cd /etc/apache2/sites-available/
+sudo a2ensite SPDXHub.conf
+
 #Install Database
 echo "Install SOCS Database..."
-mysql --user=$u --password=$p < SOCSDatabase/SQL/SPDX.sql
+mysql --user=$u --password=$p < SPDXHub/database/SPDX2.sql
+mysql --user=$u --password=$p < SPDXHub/database/testdata.sql
 #Exit mySql
 
 #Create Upload Directory
@@ -19,21 +33,11 @@ echo "Creating Upload Directory..."
 sudo mkdir SPDXHub/uploads
 sudo chmod 777 SPDXHub/uploads
 
-
 #Move source to base directorie
-echo "Reconfigure Repo Folder Structure"
-sudo mv DoSOCS/src/* DoSOCS/
-sudo mv SPDXHub/src/* SPDXHub/
-sudo chmod 777 DoSOCS/ -R
+sudo chmod 777 DoSOCS/src -R
+sudo chmod 777 SPDXHub/src -R
 
-#Remove src directories
-echo "Removing old Repo Directories"
-sudo rm DoSOCS/src -R
-sudo rm SPDXHub/src -R
-
-#Delete Database Repo
-echo "Remove Database Repo"
-sudo rm SOCSDatabase -R
+sudo service apache2 restart
 
 echo "Install Complete"
 echo "Don't forget to update the setting files ('DoSOCS/settings.py' AND 'SPDXHub/function/Data_Source.php') with the database"
