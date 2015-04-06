cfc5a4f5f7fc6c3ad9c0213c4e41d4a3bddc447d
Jacob Overkamp <jmoverkamp@gmail.com>
2015-04-06 13:19:28 -0500

    Changed query variable name.

--- src/function/spdx_doc.php
+++ src/function/spdx_doc.php
@@ -108,7 +108,7 @@ limitations under the License.
         mysql_connect("$host", "$username", "$password")or die("cannot connect server " . mysql_error());
         mysql_select_db("$db_name")or die("cannot select DB " . mysql_error());
 
-        $sql = 	"SELECT sf.spdx_pk,
+        $query = 	"SELECT sf.spdx_pk,
                          sf.document_name,
                          sf.created_date
 				FROM spdx_file sf 
@@ -133,7 +133,7 @@ limitations under the License.
 		
 		$query .= " GROUP BY document_name";		
 
-        return mysql_query($sql);
+        return mysql_query($query);
     }
 	function getLicenseVerifier1($name = "",  $spdxapproved = "",  $spdxnotapproved = "",  $notinlist = "") {
         //Create Database connection
